fa5e7a49a878ab76e25fdb2ef68aa33cf75bb72b
thegalaxydestroyer10000-pixel <thegalaxydestroyer10000@gmail.com>
2026-07-09 12:40:50 +0530

    Update solution.sql

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -1,5 +1,16 @@
--- Create Course table
+CREATE TABLE Course (
+    CourseID NUMBER(5) PRIMARY KEY,
+    CourseName VARCHAR(20) NOT NULL,
+    Credits NUMBER(2) NOT NULL,
+    DepartmentID NUMBER(5) NOT NULL
+);
 
--- Insert three records
+-- Insert 3 Records
+INSERT INTO Course VALUES (101, 'Database', 4, 1);
+INSERT INTO Course VALUES (102, 'Java', 3, 2);
+INSERT INTO Course VALUES (103, 'Networking', 3, 3);
 
--- Display structure
+-- Display Table Structures
+DESCRIBE Department;
+DESCRIBE Student;
+DESCRIBE Course;
