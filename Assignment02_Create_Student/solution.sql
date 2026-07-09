87ca37d28aea92149baed79fca8d4589e8de5af0
thegalaxydestroyer10000-pixel <thegalaxydestroyer10000@gmail.com>
2026-07-09 12:29:25 +0530

    Update solution.sql

--- Assignment02_Create_Student/solution.sql
+++ Assignment02_Create_Student/solution.sql
@@ -1,13 +1,7 @@
--- Create Student table
-
--- StudentID
-
--- StudentName
-
--- DOB
-
--- Gender
-
--- DepartmentID
-
--- Add constraints
+CREATE TABLE Student (
+    StudentID INT(5) PRIMARY KEY,
+    StudentName VARCHAR(20) NOT NULL UNIQUE,
+    DOB DATE NOT NULL,
+    Gender VARCHAR(10) NOT NULL,
+    DepartmentID INT(5) NOT NULL
+    ALTER TABLE Student
