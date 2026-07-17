99617d3a8541cbc456785fc21b7c31abbbc9d28e
thegalaxydestroyer10000-pixel <thegalaxydestroyer10000@gmail.com>
2026-07-17 11:04:19 +0530

    Update solution.sql

--- Assignment05_Insert_Student/solution.sql
+++ Assignment05_Insert_Student/solution.sql
@@ -1,9 +1,7 @@
--- Insert
-
--- 1001 Arun
-
--- 1002 Divya
-
--- 1003 Karthik
-
--- Display all records
+INSERT INTO Student (StudentID, StudentName, Gender, DepartmentID)
+VALUES 
+    (1001, 'Arun', 'Male', 101),
+    (1002, 'Divya', 'Female', 102),
+    (1003, 'Karthik', 'Male', 101);
+    
+SELECT * FROM Student;
