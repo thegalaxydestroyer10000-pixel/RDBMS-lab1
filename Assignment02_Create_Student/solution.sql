fe1e084743ac791c42c93b60ca7c4c813a83b2a3
thegalaxydestroyer10000-pixel <thegalaxydestroyer10000@gmail.com>
2026-07-17 11:06:55 +0530

    Update solution.sql

--- Assignment02_Create_Student/solution.sql
+++ Assignment02_Create_Student/solution.sql
@@ -1,7 +1,8 @@
 CREATE TABLE Student (
     StudentID INT(5) PRIMARY KEY,
-    StudentName VARCHAR(20) NOT NULL UNIQUE,
-    DOB DATE NOT NULL,
-    Gender VARCHAR(10) NOT NULL,
-    DepartmentID INT(5) NOT NULL
+    StudentName VARCHAR(20) NOT NULL,
+    DOB DATE,
+    Gender VARCHAR(10),
+    DepartmentID INT(5)
+);
 
